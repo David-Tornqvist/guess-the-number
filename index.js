f7a19bc7e58741b114d3b5dd5064bd5ecb54d976
Dmannen03 <david.tornqvist@botkyrka.skola.se>
2020-11-02 15:20:49 +0100

    commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const userInput = document.getElementById("user-input"); 
 const message = document.getElementById("message");
+const guessList = document.getElementById("guesses");
+let guesses = [];
 
 function random(min,max){
     return Math.round(Math.random()* (max - min)) + min;
@@ -16,14 +18,27 @@ function setMessage(str){
     message.innerHTML = str;
 }
 
+function clearInput(){
+    userInput.value = "";
+}
+
 function checkMessage(input){
     if(parseInt(input)  > randomN){setMessage("Too High!")};
     if(parseInt(input)  < randomN){setMessage("Too Low")};
     if(parseInt(input)  === randomN){setMessage("Correct!")};
 }
 
+function updateGuesses(){
+    guesses.push(getInput());
+    guessList.innerHTML =  "Previous guesses:" + guesses.join("</br>");
+
+}
+
 userInput.addEventListener("keyup", (event) => {
-    if(event.key === "Enter"){checkMessage(getInput())};
+    if(event.key === "Enter"){checkMessage(getInput());
+                              updateGuesses();
+                              clearInput();
+                            }
 });
 
 
